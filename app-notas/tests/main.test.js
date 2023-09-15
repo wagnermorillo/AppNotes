5cfa4b90439621f0ce29e95da9004ff640d90110
Wagner Morillo <1107665@est.intec.edu.do>
2023-09-15 17:58:46 -0400

    error

--- app-notas/tests/main.test.js
+++ app-notas/tests/main.test.js
@@ -3,7 +3,11 @@ const request = require('supertest');
 const app = require('../app');
 
 require('dotenv').config();
-const mongoURI = process.env.MONGOURI;
+
+const {MONGO_DB_USR, MONGO_DB_PWD, MONGO_DB_HOST, MONGO_DB_PORT} =
+  process.env;
+const credentials = MONGO_DB_USR ? `${MONGO_DB_USR}:${MONGO_DB_PWD}@` : '';
+const mongoURI = `mongodb://${credentials}${MONGO_DB_HOST}:${MONGO_DB_PORT}/Nota`;
 console.log(mongoURI);
 
 /* Connecting to the database before each test. */
